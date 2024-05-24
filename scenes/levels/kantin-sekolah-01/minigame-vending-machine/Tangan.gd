de23b67110b881df1f51e35735ab2b8c4939c77c
Aepu <muhammadagilfadilah@gmail.com>
2024-05-24 20:03:42 +0800

    Make hand move

--- scenes/levels/kantin-sekolah-01/minigame-vending-machine/Tangan.gd
+++ scenes/levels/kantin-sekolah-01/minigame-vending-machine/Tangan.gd
@@ -1,12 +1,27 @@
-extends Node2D
+extends CharacterBody2D
+
+@export var speed: int = 300
 
 var direction: Vector2
+
+@onready var tangan_sprite: Sprite2D = $TanganSprite
+
+@onready var hand_sprite_unhold = preload("res://assets/3.8 - Minigame vending machine/tangan1.png")
+@onready var hand_sprite_hold = preload("res://assets/3.8 - Minigame vending machine/tangan2.png")
 
 func _input(event):
 	direction = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down")).normalized()
 	
 	if event.is_action_pressed("interact"):
-		
+		tangan_sprite.texture = hand_sprite_hold
 		pass
 	elif event.is_action_released("interact"):
+		tangan_sprite.texture = hand_sprite_unhold
 		pass
+
+
+func _physics_process(delta):
+	# hand movement
+	velocity = direction * speed
+	
+	move_and_slide()
