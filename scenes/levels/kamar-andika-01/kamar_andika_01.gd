86733a97902de5d590f0899aeebfe7b4581c673e
Aepu <muhammadagilfadilah@gmail.com>
2024-05-29 22:41:58 +0800

    Membuat fungsi sementara untuk pindah scene
    Andika -> ruang tamu

--- scenes/levels/kamar-andika-01/kamar_andika_01.gd
+++ scenes/levels/kamar-andika-01/kamar_andika_01.gd
@@ -1,11 +1,10 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+
+func _on_area_2d_ke_ruang_tamu_area_entered(area):
+	print("Pindah ke ruang tamu")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_area_2d_ke_ruang_tamu_body_entered(body):
+	print("Pindah ke ruang tamu")
