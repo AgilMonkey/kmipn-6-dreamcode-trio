7186460a33d82b52a655656c760f1e74a2a916c6
azhimn <mazhn34@gmail.com>
2024-05-30 01:07:02 +0800

    minigame mencari barang

---
+++ scenes/2.3 - Minigame mencari barang/minigame_mencari_barang.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#var screen_size = get_viewport_rect().size
+	#
+	## Set the position to the center of the screen
+	#position = screen_size / 2
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
