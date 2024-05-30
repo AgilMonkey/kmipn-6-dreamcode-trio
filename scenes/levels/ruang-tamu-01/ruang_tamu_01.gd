35168676beebbad8b6fc463c70362032ea912536
Aepu <muhammadagilfadilah@gmail.com>
2024-05-30 19:53:16 +0800

    Buat agar bisa bolak balik dapur tamu

--- scenes/levels/ruang-tamu-01/ruang_tamu_01.gd
+++ scenes/levels/ruang-tamu-01/ruang_tamu_01.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var scene_kamar_andika: String = "res://scenes/levels/kamar-andika-01/kamar_andika_01.tscn"
+var scene_dapur: String = "res://scenes/levels/dapur-01/dapur_01.tscn"
 
 
 func _ready():
@@ -15,3 +16,7 @@ func _on_area_2d_to_kamar_andika_on_change_scene():
 	LevelManager.from = "RuangTamu"
 	get_tree().change_scene_to_file(scene_kamar_andika)
 
+
+func _on_area_2d_to_dapur_on_change_scene():
+	LevelManager.from = "RuangTamu"
+	get_tree().change_scene_to_file(scene_dapur)
