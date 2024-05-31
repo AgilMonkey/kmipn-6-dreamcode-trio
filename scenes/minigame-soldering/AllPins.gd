fc1de970c4be8fd73e66e22bb5ea42cad586340e
Aepu <muhammadagilfadilah@gmail.com>
2024-05-31 14:09:04 +0800

    Ngubah dikit doc nya

--- scenes/minigame-soldering/AllPins.gd
+++ scenes/minigame-soldering/AllPins.gd
@@ -8,12 +8,10 @@ var pins: Array
 func _ready():
 	pins = get_children()
 	rows = len(pins) / columns
-	
-	print(get_pins(0, 0).name)
 
 
 ## Tolong ingat row dan col awal 0
-## Cth: pin awal paling ujung adalah row=0 col=10
-func get_pins(col: int, row: int) -> Control:
+## Cth: pin awal paling ujung adalah col=9 row=0 
+func get_pin(col: int, row: int) -> Control:
 	var pin_num = col + columns * row
 	return pins[pin_num]
