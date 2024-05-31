d8d542b2c5fc62369f93007a030792c5b256ca21
Aepu <muhammadagilfadilah@gmail.com>
2024-05-31 14:04:23 +0800

    Membuat fungsi mendapatkan pin dan rows

--- scenes/minigame-soldering/AllPins.gd
+++ scenes/minigame-soldering/AllPins.gd
@@ -1,8 +1,19 @@
 extends GridContainer
 
+var rows: int
 
-var Pins: Control
+var pins: Array
 
 
 func _ready():
-	pass
+	pins = get_children()
+	rows = len(pins) / columns
+	
+	print(get_pins(0, 0).name)
+
+
+## Tolong ingat row dan col awal 0
+## Cth: pin awal paling ujung adalah row=0 col=10
+func get_pins(col: int, row: int) -> Control:
+	var pin_num = col + columns * row
+	return pins[pin_num]
