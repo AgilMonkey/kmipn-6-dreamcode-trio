853415caed03fe72791ca427bfa228a88115e543
Aepu <muhammadagilfadilah@gmail.com>
2024-05-31 14:52:23 +0800

    Make connection script

--- scenes/minigame-soldering/AllPins.gd
+++ scenes/minigame-soldering/AllPins.gd
@@ -9,6 +9,7 @@ func _init():
 	pins = get_children()
 	rows = len(pins) / columns
 	set_pin_script()
+	set_pin_connection()
 
 
 func set_pin_script():
@@ -18,8 +19,22 @@ func set_pin_script():
 	rows = len(pins) / columns
 
 
+func set_pin_connection():
+	for col in range(columns):
+		for row in range(rows):
+			var pin: Pin = get_pin(col, row)
+			pin.row_col = Vector2(row, col)
+			pin.pin_up = get_pin(pin.row_col.x - 1, pin.row_col.y)
+			pin.pin_down = get_pin(pin.row_col.x + 1, pin.row_col.y)
+			pin.pin_left = get_pin(pin.row_col.x, pin.row_col.y - 1)
+			pin.pin_right = get_pin(pin.row_col.x, pin.row_col.y + 1)
+
+
 ## Tolong ingat row dan col awal 0
 ## Cth: pin awal paling ujung adalah col=9 row=0 
-func get_pin(col: int, row: int) -> Control:
+func get_pin(col: int, row: int) -> Pin:
+	if col > columns or col < 0 or row > rows or row < 0:
+		return null
+	
 	var pin_num = col + columns * row
 	return pins[pin_num]
--- scenes/minigame-soldering/pin.gd
+++ scenes/minigame-soldering/pin.gd
@@ -1,3 +1,9 @@
 extends MarginContainer
 class_name Pin
 
+@export var row_col: Vector2
+
+@export var pin_up: Pin
+@export var pin_down: Pin
+@export var pin_left: Pin
+@export var pin_right: Pin
