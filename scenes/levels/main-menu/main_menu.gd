6cf03c9a2aeaec057aba9881a431d43501ba955a
Aepu <94035912+AgilMonkey@users.noreply.github.com>
2024-06-02 01:30:36 +0800

    Better main menu (#55)
    * Add menu asset
    
    * Add art
    
    * Added pointer asset
    
    * Bikin main menu
    
    * Add quit
    
    * Add opsi
    
    * Add setting
    
    * Membuat tentang

--- scenes/levels/main-menu/main_menu.gd
+++ scenes/levels/main-menu/main_menu.gd
@@ -2,4 +2,8 @@ extends Control
 
 
 func _on_game_baru_button_button_down():
-	get_tree().change_scene_to_file("res://scenes/levels/scene-intro/scene_intro.tscn")
+	LevelManager.change_scene_with_transition("res://scenes/levels/scene-intro/scene_intro.tscn")
+
+
+func _on_keluar_button_button_down():
+	get_tree().quit()
