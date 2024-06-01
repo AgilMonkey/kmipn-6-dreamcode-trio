a45637ab4c9607ced84c945eb947fc041b33e953
Aepu <muhammadagilfadilah@gmail.com>
2024-06-01 18:24:27 +0800

    Make cool dialogue interact di kamar andika

--- scenes/interactable/dialouge-interacting/interact_dialogue.gd
+++ scenes/interactable/dialouge-interacting/interact_dialogue.gd
@@ -7,5 +7,7 @@ extends InteractArea
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	interact = func():
-		Dialogic.start(timeline_path)
+		var layout = Dialogic.start(timeline_path)
+		layout.register_character(load("res://dialogic-stuff/characters/Andika.dch"), InteractManager.player.text_bubble_pivot)
 		await Dialogic.timeline_ended
+		InteractManager.dialogic_layout = null
