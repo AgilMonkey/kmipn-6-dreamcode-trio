37d15c86a5fd3a46200f7782457e820a4bd22857
Aepu <muhammadagilfadilah@gmail.com>
2024-06-01 23:14:16 +0800

    Bikin main menu

--- scenes/autoloads/level-manager/level_manager.gd
+++ scenes/autoloads/level-manager/level_manager.gd
@@ -9,12 +9,15 @@ var from: String  # Variabel buat memberitahu ruang sebelumnya
 var is_changing_scene := false
 
 
-func change_scene_with_transition(packed_scene: PackedScene):
+func change_scene_with_transition(scene):
 	if is_changing_scene:
 		return
 	
 	is_changing_scene = true
 	SceneTransition.start_transition(SceneTransition.FADE_TO_BLACK)
 	await SceneTransition.transition_in_middle
-	get_tree().call_deferred("change_scene_to_packed", packed_scene)
+	if scene is PackedScene:
+		get_tree().call_deferred("change_scene_to_packed", scene)
+	else:
+		get_tree().call_deferred("change_scene_to_file", scene)
 	is_changing_scene = false
--- scenes/levels/main-menu/main_menu.gd
+++ scenes/levels/main-menu/main_menu.gd
@@ -2,4 +2,4 @@ extends Control
 
 
 func _on_game_baru_button_button_down():
-	get_tree().change_scene_to_file("res://scenes/levels/scene-intro/scene_intro.tscn")
+	LevelManager.change_scene_with_transition("res://scenes/levels/scene-intro/scene_intro.tscn")
