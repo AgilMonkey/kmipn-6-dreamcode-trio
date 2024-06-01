b5ea30a12b38a7d9b4be523b253d6db51e534d7f
Aepu <muhammadagilfadilah@gmail.com>
2024-06-01 23:35:42 +0800

    Add quit

--- scenes/levels/main-menu/main_menu.gd
+++ scenes/levels/main-menu/main_menu.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_game_baru_button_button_down():
 	LevelManager.change_scene_with_transition("res://scenes/levels/scene-intro/scene_intro.tscn")
+
+
+func _on_keluar_button_button_down():
+	get_tree().quit()
