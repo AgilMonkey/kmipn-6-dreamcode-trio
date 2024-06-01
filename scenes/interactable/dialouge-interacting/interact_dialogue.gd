02dff022dafb45fd6760798a2a69e4aca13a80ac
Aepu <muhammadagilfadilah@gmail.com>
2024-06-02 04:32:35 +0800

    Removed the print and made colorful folder

--- scenes/interactable/dialouge-interacting/interact_dialogue.gd
+++ scenes/interactable/dialouge-interacting/interact_dialogue.gd
@@ -10,7 +10,6 @@ signal dialogue_started(layout)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	interact = func():
-		print(interacted_count)
 		var layout = Dialogic.start(timeline_path)
 		layout.register_character(load("res://dialogic-stuff/characters/Andika.dch"), InteractManager.player.text_bubble_pivot)
 		dialogue_started.emit(layout)
