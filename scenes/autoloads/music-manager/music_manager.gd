396e535a4870e6aed57288f3d943533d857de492
Aepu <muhammadagilfadilah@gmail.com>
2024-06-02 22:12:00 +0800

    Add music manager

---
+++ scenes/autoloads/music-manager/music_manager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+var calm_bgm := preload("res://assets/music/041415calmbgm.ogg")
+var chill_lofi_bgm := preload("res://assets/music/ChillLofiR.mp3")
+var stroking_surgery_bgm := preload("res://assets/music/Stroking Surgery.mp3")
+var pleasant_creek_bgm := preload("res://assets/music/Pleasant Creek Pack/Pleasant Creek Loop.wav")
+
+var audio_stream_player := $AudioStreamPlayer
+
+
+func play():
