9c31dc26a93da0394a91b302355795baeeedfcb2
Aepu <94035912+AgilMonkey@users.noreply.github.com>
2024-06-02 14:23:19 +0800

    Menaruh minigame dapur (#58)
    * Menaruh minigame di tempatnya
    
    * Add it to dapur and make fix bug win to early
    
    * Menambah minigamenya di dapur
    
    * Membuat dialog ayah sudah makan
    
    * Membuat tutorial makan
    
    * keluar rumah setelah makan
    
    * Disable phantom camera update

--- scenes/levels/dapur-01/dapur_01.gd
+++ scenes/levels/dapur-01/dapur_01.gd
@@ -20,3 +20,16 @@ func _on_area_2d_ke_ruang_tamu_body_entered(body):
 	SceneTransition.start_transition("FadeToBlack")
 	await SceneTransition.transition_in_middle
 	get_tree().change_scene_to_file(scene_ruang_tamu)
+
+
+func _on_minigame_makan_interactable_start_minigame(node):
+	$Player.set_process_input(false)
+	$Player.direction = Vector2.ZERO
+	$Game.visible = true
+	$Game.process_mode = Node.PROCESS_MODE_INHERIT
+	await %minigame_memakan_makanan.minigame_selesai
+	$Player.set_process_input(true)
+	$Game.visible = false
+	%minigame_memakan_makanan.process_mode = Node.PROCESS_MODE_DISABLED
+	Dialogic.VAR.sudah_makan = true
+	node.stop_minigame.emit()
